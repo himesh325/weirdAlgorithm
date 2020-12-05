d7810a51a7ada8e4e81fe7541a2598fc06178a56
himesh kumar <yankee_fivesix@hotmail.com>
2020-12-05 11:37:03 +0530

    Add project files.

---
+++ weirdAlgorithm/weirdAlgorithm.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+
+	cin >> n;
+	cout << endl;
+
+	while (n != 1)
+	{
+		if (n % 2 == 0)
+		{
+			n = n / 2;
+		}
+		else
+		{
+			n = n * 3 + 1;
+
+		}
+
+		cout << n;
+		cout << "\t";
+	}
+
+	return 0;
+}
